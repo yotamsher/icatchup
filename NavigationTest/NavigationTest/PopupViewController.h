c619d830d60d1ab3cdbaa0103f8c78dac3524ab6
Yotam Sher <yotamsher@Yotams-MacBook-Pro.local>
2014-12-24 23:44:51 +0200

    with dismiss

---
+++ NavigationTest/NavigationTest/PopupViewController.h
@@ -0,0 +1,14 @@
+//
+//  PopupViewController.h
+//  NavigationTest
+//
+//  Created by Yotam Sher on 12/24/14.
+//  Copyright (c) 2014 MyTests. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PopupViewController : UIViewController
+- (IBAction)Done:(id)sender;
+
+@end
---
+++ NavigationTest/NavigationTest/PopupViewController.m
@@ -0,0 +1,16 @@
+//
+//  PopupViewController.m
+//  NavigationTest
+//
+//  Created by Yotam Sher on 12/24/14.
+//  Copyright (c) 2014 MyTests. All rights reserved.
+//
+
+#import "PopupViewController.h"
+
+@implementation PopupViewController
+
+- (IBAction)Done:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
